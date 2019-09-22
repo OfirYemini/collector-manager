33f37a1b3b1651d2c01aa1f7b102dc15adb89b5c
ofir@webeyez.com <ofir@webeyez.com>
2019-09-22 20:14:45 +0300

    fix query string parameters

--- src/services/transactions/list.js
+++ src/services/transactions/list.js
@@ -1,15 +1,19 @@
 const { Client } = require('pg');  //  Needs the nodePostgres Lambda Layer.
 exports.handler = async (event) => {
-    if(!(event && event.query && event.query.from && event.query.to)){
+    console.log('event.query ',event.query)
+    if(!(event && event.queryStringParameters && event.queryStringParameters.from && event.queryStringParameters.to)){
         return {
             statusCode: 400,
+            headers: {
+                "Content-Type": "text/plain"
+              },
             body: 'no dates variables',
         };
     }
 
     const client = new Client();  
     await client.connect();
-    const res = await client.query(`select * from transactions where date < $1 and date > $2;`,new Date(event.query.from),new Date(event.query.to));
+    const res = await client.query(`select * from transactions where date < $1 and date > $2;`,new Date(event.queryStringParameters.from),new Date(event.queryStringParameters.to));
     await client.end();  
 
     const response = {
