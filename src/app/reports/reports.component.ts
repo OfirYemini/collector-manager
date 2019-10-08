4bdfd982dde0348af1a14397199b6108c6394a29
ofir@webeyez.com <ofir@webeyez.com>
2019-10-08 09:14:53 +0300

    integrate heb date

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -11,34 +11,50 @@ import Hebcal from 'hebcal';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
-  periods: {startMonth:number,endMonth:number,startDay:number,endDay:number,text:string}[];
+  periods: {startMonth:number,endMonth:number,startDay:number,endDay:number,startText:string,endText:string}[];
   years: {value:number,text:string}[] = [];
-
+  gregToHebMonth:any = {
+    "Nisan":"ניסן",
+    "Iyyar":"אייר",
+    "Sivan":"סיון",
+    "Tamuz":"תמוז",
+    "Av":"אב",
+    "Elul":"אלול",
+    "Tishrei":"תשרי",
+    "Cheshvan":"חשון",
+    "Kislev":"כסלו",
+    "Tevet":"טבת",
+    "Sh'vat":"שבט"
+  };
+    
   //hebrewDate:Observable<any>;
   hebrewDate: any;
   eofDateTs: number;
   reports: any[];
   users: any = {};
   selectedPeriodIndex:number;
-  selectedYear:number = 0;
+  selectedYear:{value:number,text:string};
 
   constructor(route: ActivatedRoute, private reportService: ReportsService,  private usersService: UsersService) {
     var currentYear = new Hebcal.HDate(new Date()).year;
     for (var i = 0; i <= 3; i++) {
       this.years[i] = {value:currentYear - i,text:Hebcal.gematriya(currentYear - i)};
     }
+    this.selectedYear = this.years[0];
     this.periods = [{
       startMonth:7,
       startDay:1,
       endMonth:1,
       endDay:14,
-      text:'ראש השנה עד ערב פסח'
+      startText:'ראש השנה' ,
+      endText:'ערב פסח'
     },{
       startMonth:1,
       startDay:15,
       endMonth:6,
       endDay:29,
-      text:'פסח עד ערב ראש השנה'
+      startText:'פסח' ,
+      endText:'ערב ראש השנה'
     }]
   }
 
@@ -56,12 +72,19 @@ export class ReportsComponent implements OnInit {
     let selectedPeriod = this.periods[this.selectedPeriodIndex];
     
     //var day = new Hebcal.HDate(7, 2, 5770);
-    var from = new Hebcal.HDate(selectedPeriod.startDay, selectedPeriod.startMonth,this.selectedYear); //ערב פסח
-    var until = new Hebcal.HDate(selectedPeriod.endDay, selectedPeriod.endMonth,this.selectedYear); 
-    
+    var from = new Hebcal.HDate(selectedPeriod.startDay, selectedPeriod.startMonth,this.selectedYear.value); //ערב פסח
+    var until = new Hebcal.HDate(selectedPeriod.endDay, selectedPeriod.endMonth,this.selectedYear.value); 
+    var HebcalDate = Hebcal.HDate;
     this.reportService.getAllReports(from.greg(), until.greg()).subscribe((data: any[]) => {
       console.log(data);
-      this.reports = data;
+      this.reports = data.map((r)=>{
+        r.transactions = r.transactions.map((t)=>{
+          let hebDate = new HebcalDate(new Date(t.date));
+          t.date = Hebcal.gematriya(hebDate.getDate()) + ' ' + this.gregToHebMonth[hebDate.getMonthName()];
+          return t;
+        },this);
+        return r;
+      },this);
     });
 
   }
