5b9fde522eb2dbbef566de0f605ff860f45f1628
ofir@webeyez.com <ofir@webeyez.com>
2019-10-09 22:11:02 +0300

    fix users v1

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import { UsersService } from '../users.service';
-import { Component, OnInit,ViewChild  } from '@angular/core';
-import { MatDialog, MatTable } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatTable, MatPaginator, MatTableDataSource } from '@angular/material';
 import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
 
 @Component({
@@ -9,59 +9,69 @@ import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  users:any[]
-  displayedColumns = ['id','lastName','firstName','email','isGuest','action'];
-  @ViewChild(MatTable,{static:true}) table: MatTable<any>;
+  users: User[]
+  displayedColumns = ['id', 'lastName', 'firstName', 'email', 'isGuest', 'action'];
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
   action: string;
   updatedUserId: number;
-  newRow:any = {isGuest:false};
-  
-
-  constructor(private usersService:UsersService, public dialog: MatDialog) { }
+  newRow: any = { isGuest: false };
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  dataSource:MatTableDataSource<User>;
+  constructor(private usersService: UsersService, public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.usersService.getUsers().subscribe((data : any[])=>{
-      console.log('ngOnInit ',data);
-      this.users = data.filter(u=>u.isActive);
+    this.usersService.getUsers().subscribe((data: any[]) => {
+      console.log('ngOnInit ', data);
+      this.users = data.filter(u => u.isActive);      
+      this.dataSource = new MatTableDataSource<User>(this.users);
+      this.dataSource.paginator = this.paginator;
     });
   }
 
-  refreshUsers(){
-    this.usersService.getUsers().subscribe((data : any[])=>{
+  refreshUsers() {
+    this.usersService.getUsers().subscribe((data: any[]) => {
       console.log('refreshUsers ', data);
-      this.users = data;      
-      //this.table.renderRows();
+      this.users = data.filter(u => u.isActive);
+      this.table.renderRows();
     });
   }
-  setAction(action,obj) {
+  setAction(action, obj) {
     this.action = action;
     this.updatedUserId = obj.id;
   }
-  addUser(){
-    this.usersService.addUser(this.newRow).subscribe((data : any)=>{
+  addUser() {
+    this.usersService.addUser(this.newRow).subscribe((data: any) => {
       this.refreshUsers();
       this.action = null;
       this.updatedUserId = null;
       this.newRow = {};
-    },err=>console.log('error adding user',err));
+    }, err => console.log('error adding user', err));
   }
-  saveUser(row){
-    this.usersService.updateUser(row.id,{
-      firstName:row.firstName,
-      lastName:row.lastName,
-      email:row.email,
-      isGuest:row.isGuest
-    }).subscribe((data : any)=>{
+  saveUser(row) {
+    this.usersService.updateUser(row.id, {
+      firstName: row.firstName,
+      lastName: row.lastName,
+      email: row.email,
+      isGuest: row.isGuest
+    }).subscribe((data: any) => {
       this.refreshUsers();
       this.action = null;
       this.updatedUserId = null;
-    },err=>console.log('error saving user',err));
+    }, err => console.log('error saving user', err));
   }
-  deleteUser(userId:number){
-    this.usersService.deleteUser(userId).subscribe(()=>{
+  deleteUser(userId: number) {
+    this.usersService.deleteUser(userId).subscribe(() => {
       this.refreshUsers();
       this.action = null;
       this.updatedUserId = null;
-    },err=>console.log('error removing user',err));
+    }, err => console.log('error removing user', err));
   }
+}
+
+export interface User {
+  id:number,
+  firstName: string,
+  lastName: string,
+  email: string,
+  isGuest: boolean
 }
