e76e5169534f8bddedb47053ede7811202d171b7
ofir@webeyez.com <ofir@webeyez.com>
2019-10-22 23:54:37 +0300

    authenticate using cognito identity

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,53 +1,71 @@
 import { Injectable } from '@angular/core';
-import {CognitoAuth} from 'amazon-cognito-auth-js'
+import {  CognitoUserPool } from 'amazon-cognito-identity-js'
+import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
-
-  public isAuthenticated(){
-    return false;
-  }
-
-  public signOut(){
-    console.log('todo');
-  }
-  public signIn(){
+  
+  private _userPool:CognitoUserPool;
+  constructor() {
     var authData = {
-      ClientId : 'tqopm6bgbqm90nq5iipg2a7b7', // Your client id here
-      AppWebDomain : 'gabay-notebook.auth.eu-central-1.amazoncognito.com',
-      TokenScopesArray : ['phone', 'email', 'profile','openid'],
-      RedirectUriSignIn : 'https://gabay-notebook.com/users',
-      RedirectUriSignOut : 'https://gabay-notebook.com',
+      ClientId: 'tqopm6bgbqm90nq5iipg2a7b7', // Your client id here
+      AppWebDomain: 'gabay-notebook.auth.eu-central-1.amazoncognito.com',
+      TokenScopesArray: ['phone', 'email', 'profile', 'openid'],
+      RedirectUriSignIn: 'https://gabay-notebook.com/users',
+      RedirectUriSignOut: 'https://gabay-notebook.com',
       //IdentityProvider : '<TODO: add identity provider you want to specify>', // e.g. 'Facebook',
-      UserPoolId : 'eu-central-1_kbheFh6SU', // Your user pool id here
+      UserPoolId: 'eu-central-1_kbheFh6SU', // Your user pool id here
       //AdvancedSecurityDataCollectionFlag : '<TODO: boolean value indicating whether you want to enable advanced security data collection>', // e.g. true
       //Storage: '<TODO the storage object>' // OPTIONAL e.g. new CookieStorage(), to use the specified storage provided
     };
-    var auth = new CognitoAuth(authData);
+    let data = {
+      UserPoolId: 'eu-central-1_kbheFh6SU', // your user pool id here
+      ClientId: 'tqopm6bgbqm90nq5iipg2a7b7'
+    };
+    this._userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+    
 
-    return Observable.create(observer=>{
-      auth.userhandler = {			
-        onSuccess: function(result) {
-          console.log("Sign in success",result);
-          //showSignedIn(result);
-          observer.next(result);
-          observer.complete();
-        },
-        onFailure: function(err) {
-          console.log("Error!" + err);
-          observer.error(err);
-        }
+  }
+
+  public isAuthenticated() {
+    return false;
+  }
+
+  public signOut() {
+    //this._auth.signOut();
+  }
+  public signIn() {
+    //this._auth.setState();
+    return Observable.create(observer => {
+      var userData = {
+        Username: 'ofiry1981@gmail.com', // your username here
+        Pool: this._userPool
       };
-      auth.getSession();
-    });
+      var authenticationData = {
+        Username: 'ofiry1981@gmail.com', // your username here
+        Password: 'Aa123456@', // your password here
+      };
+      var authenticationDetails =
+        new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
-    
-    //auth.setState(<state parameter>);
-    
+      var cognitoUser =
+        new AmazonCognitoIdentity.CognitoUser(userData);
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+          var accessToken = result.getAccessToken().getJwtToken();
+        },
+
+        onFailure: function (err) {
+          alert(err);
+        },
+        mfaRequired: function (codeDeliveryDetails) {
+          var verificationCode = prompt('Please input verification code', '');
+          cognitoUser.sendMFACode(verificationCode, this);
+        }
+      });
+    });
   }
 }
